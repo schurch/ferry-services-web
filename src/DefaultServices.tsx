8b3653cd65bcdb882be3fae9784a0d6e9b6d751d
Stefan Church <stefan.church@gmail.com>
2022-05-07 11:44:56 +1200

    Update default services

--- src/DefaultServices.tsx
+++ src/DefaultServices.tsx
@@ -13,15 +13,18 @@ export let defaultServices = [
         "latitude": 56.68855,
         "name": "Kilchoan",
         "id": 14,
+        "weather": null,
         "longitude": -6.09396
       },
       {
         "latitude": 56.6233,
         "name": "Tobermory",
         "id": 13,
+        "weather": null,
         "longitude": -6.06334
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -37,15 +40,18 @@ export let defaultServices = [
         "latitude": 55.576606,
         "name": "Brodick",
         "id": 4,
+        "weather": null,
         "longitude": -5.139172
       },
       {
         "latitude": 55.640516,
         "name": "Ardrossan",
         "id": 3,
+        "weather": null,
         "longitude": -4.823062
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -61,21 +67,25 @@ export let defaultServices = [
         "latitude": 55.75106,
         "name": "Claonaig",
         "id": 52,
+        "weather": null,
         "longitude": -5.38787
       },
       {
         "latitude": 55.707714,
         "name": "Lochranza",
         "id": 16,
+        "weather": null,
         "longitude": -5.301985
       },
       {
         "latitude": 55.86615,
         "name": "Tarbert (Loch Fyne)",
         "id": 15,
+        "weather": null,
         "longitude": -5.40376
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -91,21 +101,25 @@ export let defaultServices = [
         "latitude": 57.152201,
         "name": "Lochboisdale",
         "id": 21,
+        "weather": null,
         "longitude": -7.303969
       },
       {
         "latitude": 56.953846,
         "name": "Castlebay",
         "id": 20,
+        "weather": null,
         "longitude": -7.488251
       },
       {
         "latitude": 56.41158,
         "name": "Oban",
         "id": 19,
+        "weather": null,
         "longitude": -5.47725
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -121,15 +135,18 @@ export let defaultServices = [
         "latitude": 57.07091,
         "name": "Eriskay",
         "id": 18,
+        "weather": null,
         "longitude": -7.30825
       },
       {
         "latitude": 57.0084,
         "name": "Barra",
         "id": 17,
+        "weather": null,
         "longitude": -7.40475
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -145,15 +162,18 @@ export let defaultServices = [
         "latitude": 55.92067,
         "name": "Rhubodach",
         "id": 23,
+        "weather": null,
         "longitude": -5.15888
       },
       {
         "latitude": 55.923353,
         "name": "Colintraive",
         "id": 22,
+        "weather": null,
         "longitude": -5.152758
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -169,15 +189,18 @@ export let defaultServices = [
         "latitude": 55.83848,
         "name": "Rothesay",
         "id": 8,
+        "weather": null,
         "longitude": -5.05421
       },
       {
         "latitude": 55.87573,
         "name": "Wemyss Bay",
         "id": 7,
+        "weather": null,
         "longitude": -4.8908
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -193,21 +216,25 @@ export let defaultServices = [
         "latitude": 56.508324,
         "name": "Tiree",
         "id": 25,
+        "weather": null,
         "longitude": -6.798924
       },
       {
         "latitude": 56.615059,
         "name": "Coll",
         "id": 24,
+        "weather": null,
         "longitude": -6.524241
       },
       {
         "latitude": 56.41158,
         "name": "Oban",
         "id": 19,
+        "weather": null,
         "longitude": -5.47725
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -223,27 +250,32 @@ export let defaultServices = [
         "latitude": 55.8067,
         "name": "Kennacraig",
         "id": 28,
+        "weather": null,
         "longitude": -5.4834
       },
       {
         "latitude": 55.847707,
         "name": "Port Askaig",
         "id": 27,
+        "weather": null,
         "longitude": -6.105102
       },
       {
         "latitude": 56.06858,
         "name": "Colonsay",
         "id": 26,
+        "weather": null,
         "longitude": -6.18819
       },
       {
         "latitude": 56.41158,
         "name": "Oban",
         "id": 19,
+        "weather": null,
         "longitude": -5.47725
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -259,15 +291,18 @@ export let defaultServices = [
         "latitude": 55.87649,
         "name": "Portavadie",
         "id": 29,
+        "weather": null,
         "longitude": -5.31654
       },
       {
         "latitude": 55.86615,
         "name": "Tarbert (Loch Fyne)",
         "id": 15,
+        "weather": null,
         "longitude": -5.40376
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -283,15 +318,18 @@ export let defaultServices = [
         "latitude": 55.947006,
         "name": "Dunoon",
         "id": 57,
+        "weather": null,
         "longitude": -4.921328
       },
       {
         "latitude": 55.959938,
         "name": "Gourock",
         "id": 56,
+        "weather": null,
         "longitude": -4.814372
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -307,15 +345,18 @@ export let defaultServices = [
         "latitude": 55.78671,
         "name": "Cumbrae Slip",
         "id": 12,
+        "weather": null,
         "longitude": -4.898246
       },
       {
         "latitude": 55.794945,
         "name": "Largs",
         "id": 11,
+        "weather": null,
         "longitude": -4.871013
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -331,15 +372,18 @@ export let defaultServices = [
         "latitude": 55.678637,
         "name": "Gigha",
         "id": 31,
+        "weather": null,
         "longitude": -5.733597
       },
       {
         "latitude": 55.65755,
         "name": "Tayinloan",
         "id": 30,
+        "weather": null,
         "longitude": -5.6691
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -355,15 +399,18 @@ export let defaultServices = [
         "latitude": 57.896848,
         "name": "Tarbert",
         "id": 33,
+        "weather": null,
         "longitude": -6.798668
       },
       {
         "latitude": 57.58634,
         "name": "Uig",
         "id": 32,
+        "weather": null,
         "longitude": -6.376447
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -379,15 +426,18 @@ export let defaultServices = [
         "latitude": 56.330225,
         "name": "Iona",
         "id": 35,
+        "weather": null,
         "longitude": -6.392257
       },
       {
         "latitude": 56.325611,
         "name": "Fionnphort",
         "id": 34,
+        "weather": null,
         "longitude": -6.369403
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -403,21 +453,25 @@ export let defaultServices = [
         "latitude": 55.62781,
         "name": "Port Ellen",
         "id": 36,
+        "weather": null,
         "longitude": -6.18981
       },
       {
         "latitude": 55.8067,
         "name": "Kennacraig",
         "id": 28,
+        "weather": null,
         "longitude": -5.4834
       },
       {
         "latitude": 55.847707,
         "name": "Port Askaig",
         "id": 27,
+        "weather": null,
         "longitude": -6.105102
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -433,15 +487,18 @@ export let defaultServices = [
         "latitude": 56.399859,
         "name": "Kerrera",
         "id": 55,
+        "weather": null,
         "longitude": -5.517498
       },
       {
         "latitude": 56.396995,
         "name": "Gallanach",
         "id": 54,
+        "weather": null,
         "longitude": -5.510373
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -457,15 +514,45 @@ export let defaultServices = [
         "latitude": 55.984704635223416,
         "name": "Kilcreggan",
         "id": 58,
+        "weather": null,
         "longitude": -4.820426740646081
       },
       {
         "latitude": 55.959938,
         "name": "Gourock",
         "id": 56,
+        "weather": null,
         "longitude": -4.814372
       }
-    ]
+    ],
+    "vessels": []
+  },
+  {
+    "status": -99,
+    "area": "KINTYRE",
+    "additional_info": null,
+    "last_updated_date": null,
+    "route": "Ardrossan (ARD) - Campbeltown (CAM)",
+    "service_id": 36,
+    "updated": null,
+    "sort_order": 19,
+    "locations": [
+      {
+        "latitude": 55.424047,
+        "name": "Campbeltown",
+        "id": 53,
+        "weather": null,
+        "longitude": -5.599388
+      },
+      {
+        "latitude": 55.640516,
+        "name": "Ardrossan",
+        "id": 3,
+        "weather": null,
+        "longitude": -4.823062
+      }
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -475,21 +562,24 @@ export let defaultServices = [
     "route": "Ullapool (ULL) - Stornoway (STO)",
     "service_id": 25,
     "updated": null,
-    "sort_order": 19,
+    "sort_order": 20,
     "locations": [
       {
         "latitude": 57.894939,
         "name": "Ullapool Bay",
         "id": 10,
+        "weather": null,
         "longitude": -5.160442
       },
       {
         "latitude": 58.206822,
         "name": "Stornoway",
         "id": 9,
+        "weather": null,
         "longitude": -6.386586
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -499,21 +589,24 @@ export let defaultServices = [
     "route": "Freight Ullapool -Stornoway ",
     "service_id": 35,
     "updated": null,
-    "sort_order": 20,
+    "sort_order": 21,
     "locations": [
       {
         "latitude": 57.894939,
         "name": "Ullapool Bay",
         "id": 10,
+        "weather": null,
         "longitude": -5.160442
       },
       {
         "latitude": 58.206822,
         "name": "Stornoway",
         "id": 9,
+        "weather": null,
         "longitude": -6.386586
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -523,21 +616,24 @@ export let defaultServices = [
     "route": "Oban (OBA) - Lismore (LIS)",
     "service_id": 15,
     "updated": null,
-    "sort_order": 21,
+    "sort_order": 22,
     "locations": [
       {
         "latitude": 56.511159,
         "name": "Lismore",
         "id": 37,
+        "weather": null,
         "longitude": -5.492068
       },
       {
         "latitude": 56.41158,
         "name": "Oban",
         "id": 19,
+        "weather": null,
         "longitude": -5.47725
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -547,45 +643,52 @@ export let defaultServices = [
     "route": "Lochaline (LAL) - Fishnish (FIS)",
     "service_id": 12,
     "updated": null,
-    "sort_order": 22,
+    "sort_order": 23,
     "locations": [
       {
         "latitude": 56.51472,
         "name": "Fishnish",
         "id": 51,
+        "weather": null,
         "longitude": -5.81032
       },
       {
         "latitude": 56.53666,
         "name": "Lochaline",
         "id": 50,
+        "weather": null,
         "longitude": -5.77502
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
     "area": "MULL",
     "additional_info": null,
     "last_updated_date": null,
+    "disruption_reason": "technical",
     "route": "Oban (OBA) - Craignure (CRA)",
     "service_id": 11,
     "updated": null,
-    "sort_order": 23,
+    "sort_order": 24,
     "locations": [
       {
         "latitude": 56.47074,
         "name": "Craignure",
         "id": 38,
+        "weather": null,
         "longitude": -5.70629
       },
       {
         "latitude": 56.41158,
         "name": "Oban",
         "id": 19,
+        "weather": null,
         "longitude": -5.47725
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -595,21 +698,24 @@ export let defaultServices = [
     "route": "Uig, Skye (UIG) - Lochmaddy (LMA)",
     "service_id": 22,
     "updated": null,
-    "sort_order": 24,
+    "sort_order": 25,
     "locations": [
       {
         "latitude": 57.596518,
         "name": "Lochmaddy",
         "id": 39,
+        "weather": null,
         "longitude": -7.157672
       },
       {
         "latitude": 57.58634,
         "name": "Uig",
         "id": 32,
+        "weather": null,
         "longitude": -6.376447
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -619,21 +725,24 @@ export let defaultServices = [
     "route": "Berneray (BER) - Leverburgh (LEV)",
     "service_id": 23,
     "updated": null,
-    "sort_order": 25,
+    "sort_order": 26,
     "locations": [
       {
         "latitude": 57.76654,
         "name": "Leverburgh",
         "id": 41,
+        "weather": null,
         "longitude": -7.025258
       },
       {
         "latitude": 57.702302,
         "name": "Berneray",
         "id": 40,
+        "weather": null,
         "longitude": -7.180422
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -642,39 +751,46 @@ export let defaultServices = [
     "route": "Scrabster - Stromness / Aberdeen - Kirkwall - Lerwick",
     "service_id": 1000,
     "updated": null,
-    "sort_order": 26,
+    "sort_order": 27,
     "locations": [
       {
         "latitude": 60.16270380403575,
         "name": "Lerwick",
         "id": 63,
+        "weather": null,
         "longitude": -1.1589239389948884
       },
       {
         "latitude": 58.99989823953207,
         "name": "Kirkwall",
         "id": 62,
+        "weather": null,
         "longitude": -2.9742511887813246
       },
       {
         "latitude": 57.145172030019346,
         "name": "Aberdeen",
         "id": 61,
+        "weather": null,
         "longitude": -2.091230925036305
       },
       {
         "latitude": 58.96421632953152,
         "name": "Stromness",
         "id": 60,
+        "weather": null,
         "longitude": -3.2954037645564296
       },
       {
         "latitude": 58.61252430922174,
         "name": "Scrabster",
         "id": 59,
+        "weather": null,
         "longitude": -3.545534457670658
       }
-    ]
+    ],
+    "vessels": [],
+    "last_updated_date": null
   },
   {
     "status": -99,
@@ -684,45 +800,52 @@ export let defaultServices = [
     "route": "Sconser (SCO) - Raasay (RAA)",
     "service_id": 17,
     "updated": null,
-    "sort_order": 27,
+    "sort_order": 28,
     "locations": [
       {
         "latitude": 57.351034,
         "name": "Raasay",
         "id": 43,
+        "weather": null,
         "longitude": -6.082454
       },
       {
         "latitude": 57.313967,
         "name": "Sconser",
         "id": 42,
+        "weather": null,
         "longitude": -6.110329
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
     "area": "SKYE",
     "additional_info": null,
     "last_updated_date": null,
+    "disruption_reason": "operational",
     "route": "Mallaig (MAL) - Armadale (ARM)",
     "service_id": 18,
     "updated": null,
-    "sort_order": 28,
+    "sort_order": 29,
     "locations": [
       {
         "latitude": 57.064596,
         "name": "Armadale",
         "id": 45,
+        "weather": null,
         "longitude": -5.894743
       },
       {
         "latitude": 57.006834,
         "name": "Mallaig",
         "id": 44,
+        "weather": null,
         "longitude": -5.828069
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -732,39 +855,45 @@ export let defaultServices = [
     "route": "Mallaig (MAL) - Small Isles (SIS)",
     "service_id": 19,
     "updated": null,
-    "sort_order": 29,
+    "sort_order": 30,
     "locations": [
       {
         "latitude": 57.056171,
         "name": "Canna",
         "id": 49,
+        "weather": null,
         "longitude": -6.490296
       },
       {
         "latitude": 57.010862,
         "name": "Rum",
         "id": 48,
+        "weather": null,
         "longitude": -6.264991
       },
       {
         "latitude": 56.83364,
         "name": "Muck",
         "id": 47,
+        "weather": null,
         "longitude": -6.226905
       },
       {
         "latitude": 56.8772,
         "name": "Eigg",
         "id": 46,
+        "weather": null,
         "longitude": -6.12995
       },
       {
         "latitude": 57.006834,
         "name": "Mallaig",
         "id": 44,
+        "weather": null,
         "longitude": -5.828069
       }
-    ]
+    ],
+    "vessels": []
   },
   {
     "status": -99,
@@ -774,20 +903,23 @@ export let defaultServices = [
     "route": "Mallaig (MAL) - Lochboisdale (LBO)",
     "service_id": 37,
     "updated": null,
-    "sort_order": 30,
+    "sort_order": 31,
     "locations": [
       {
         "latitude": 57.006834,
         "name": "Mallaig",
         "id": 44,
+        "weather": null,
         "longitude": -5.828069
       },
       {
         "latitude": 57.152201,
         "name": "Lochboisdale",
         "id": 21,
+        "weather": null,
         "longitude": -7.303969
       }
-    ]
+    ],
+    "vessels": []
   }
 ]
